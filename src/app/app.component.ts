78e3bfdbc822368f2589ecc586ec23d7f901011b
Jeremi <kdrisaac42@gmail.com>
2021-01-29 10:05:25 +0100

    cleaning ui for poc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,37 +13,39 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'Geolocation',
+      url: '/folder/Geolocation',
+      icon: 'map'
     },
-    {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
-    },
-    {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
-    }
+    // {
+    //   title: 'Outbox',
+    //   url: '/folder/Outbox',
+    //   icon: 'paper-plane'
+    // },
+    // {
+    //   title: 'Favorites',
+    //   url: '/folder/Favorites',
+    //   icon: 'heart'
+    // },
+    // {
+    //   title: 'Archived',
+    //   url: '/folder/Archived',
+    //   icon: 'archive'
+    // },
+    // {
+    //   title: 'Trash',
+    //   url: '/folder/Trash',
+    //   icon: 'trash'
+    // },
+    // {
+    //   title: 'Spam',
+    //   url: '/folder/Spam',
+    //   icon: 'warning'
+    // }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = [
+    'Geolocation', 'POC'
+  ];
 
   constructor(
     private platform: Platform,
