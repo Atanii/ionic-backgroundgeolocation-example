1c4615e7703870d6b05f13432e59c7757fc9c378
Kaposvári Dániel <daniel.kaposvari@griffsoft.hu>
2021-02-15 16:22:32 +0100

    cleaning code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,15 +17,6 @@ import { BackgroundMode } from '@ionic-native/background-mode/ngx';
 
 const { Geolocation } = Plugins;
 
-interface GeofenceObject {
-  id: string,
-  latitude: number,
-  longitude: number,
-  radius: number,
-  transitionType: 1 | 2 | 3,
-  notification?: object
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -88,19 +79,6 @@ export class AppComponent implements OnInit, OnDestroy {
     saveBatteryOnBackground: false
   };
 
-  readonly geolocationConfig: GeolocationOptions = {
-    enableHighAccuracy: true,
-    timeout: 5000
-  }
-
-  readonly watchedGeofence: GeofenceObject = {
-    id: 'teszt-geofence',
-    latitude: 0,
-    longitude: 0,
-    radius: 100,
-    transitionType: 2
-  }
-
   setBg: boolean;
   setBgGeoLoc: boolean;
   setPOST: boolean;
@@ -108,7 +86,6 @@ export class AppComponent implements OnInit, OnDestroy {
   setMiscLog: boolean;
   setGeoLog: boolean;
   setErrorLog: boolean;
-  setWakelock: boolean;
 
   constructor(
     private platform: Platform,
@@ -128,48 +105,6 @@ export class AppComponent implements OnInit, OnDestroy {
     } else {
       this.backgroundMode.disable();
     }
-  }
-
-  onWakelockSettingsUpdated() {
-    // Aquire
-    /*if (this.setWakelock) {
-      this.powerManagement.acquire()
-        .then(res => {
-          console.log('[INFO][POWER] Wakelock acquired: ', res);
-        })
-        .catch(err => {
-          console.log('[INFO][POWER] Failed to acquire wakelock: ', err);
-        });
-
-      this.powerManagement.setReleaseOnPause(false).then(
-        _ => {
-          console.log('setReleaseOnPause successfully');
-        },
-        () => {
-          console.log('Failed to set');
-        }
-      );
-
-      // CRASHES APPLICATION AFTER A FEW SEC
-      // this.powerManagement.dim().then(
-      //   _ => {
-      //     console.log('Wakelock acquired');
-      //   },
-      //   err => {
-      //     console.log('Failed to acquire wakelock: ', err);
-      //   }
-      // );
-    }
-    // Release
-    else {
-      this.powerManagement.release()
-      .then(res => {
-          console.log('[INFO][POWER] Wakelock release: ', res);
-        })
-      .catch(err => {
-          console.log('[INFO][POWER] Failed to release wakelock: ', err);
-      });
-    }*/
   }
 
   onBgGeoLocSettingsUpdated() {
@@ -194,13 +129,9 @@ export class AppComponent implements OnInit, OnDestroy {
       this.setMiscLog = false;
       this.setGeoLog = false;
       this.setErrorLog = false;
-      this.setWakelock = false;
 
       this.initAndConfigureBgGeoLoc();
-      // this.initAndConfigureGeofencing();
-
       this.onBgModeSettingUpdated();
-      this.onWakelockSettingsUpdated();
       this.onBgGeoLocSettingsUpdated();
     });
   }
@@ -231,7 +162,6 @@ export class AppComponent implements OnInit, OnDestroy {
                 appTime: new Date(),
                 content: 'Background mode'
               } as SimpleMessage);
-              //this.backgroundGeolocation.finish();
             }
           );
 
@@ -259,7 +189,6 @@ export class AppComponent implements OnInit, OnDestroy {
               }
 
               ///////////////////////////////////////// POST
-              // this.backgroundGeolocation.finish();
               // this.locService.postNewLocation({
               //   pluginTime: new Date(location.time),
               //   appTime: new Date(),
@@ -267,34 +196,10 @@ export class AppComponent implements OnInit, OnDestroy {
               //   latitude: location.latitude,
               //   msg: 'BackgroundGeolocationEvents.location'
               // } as GeoLoc);
-              // this.backgroundGeolocation.finish();
 
               //this.backgroundGeolocation.finish();
             }
           );
-
-        // this.backgroundGeolocation.headlessTask(function (event) {
-        //   if (event.name === 'location' ||
-        //       event.name === 'stationary') {
-        //     console.log("[INFO] TASK: ", event);
-
-        //     ///////////////////////////////////////// POST
-        //     if (this.setPOST) {
-        //       this.locService.postData(event.params);
-        //     }
-
-        //     if (this.setLog) {
-        //       this.locService.addNewMsg({
-        //         pluginTime: new Date(),
-        //         appTime: new Date(),
-        //         content: 'Headless: ' + JSON.stringify(event.params)
-        //       } as SimpleMessage
-        //       );
-        //     }
-        //   }
-
-        //   return 'Processing event: ' + event.name; // will be logged
-        // });
 
         this.backgroundGeolocation.on(BackgroundGeolocationEvents.stationary)
           .pipe(
@@ -310,44 +215,8 @@ export class AppComponent implements OnInit, OnDestroy {
                   latitude: location.latitude
                 } as GeoLoc);
               }
-              //this.backgroundGeolocation.finish();
             }
           );
-
-        //   this.backgroundGeolocation.getStationaryLocation().then(res => {
-        //     if (res && res.longitude !== undefined) {
-        //       const stats = `[INFO][TIMER] getStationaryLocation(),\n time: ${new Date(res.time)}, appTime: ${new Date()},\n long: ${res.longitude}, lat: ${res.latitude}`
-        //       console.log(stats);
-
-        //       if (this.setGeoLog) {
-        //         this.locService.addNewLoc({
-        //           pluginTime: new Date(res.time),
-        //           appTime: new Date(),
-        //           longitude: res.longitude,
-        //           latitude: res.latitude
-        //         } as GeoLoc);
-        //       }
-
-        //       if (this.setLog) {
-        //         this.locService.addNewMsg({
-        //           pluginTime: new Date(res.time),
-        //           appTime: new Date(),
-        //           content: stats
-        //         } as SimpleMessage);
-        //       }
-
-        //       ///////////////////////////////////////// POST
-        //       // this.locService.postNewLocation({
-        //       //   pluginTime: new Date(res.time),
-        //       //   appTime: new Date(),
-        //       //   longitude: res.longitude,
-        //       //   latitude: res.latitude,
-        //       //   msg: 'timer -> stationarylocation'
-        //       // } as GeoLoc);
-        //     }
-        //   })
-
-        // });
 
         this.backgroundGeolocation.on(BackgroundGeolocationEvents.error)
           .pipe(
@@ -362,13 +231,6 @@ export class AppComponent implements OnInit, OnDestroy {
                 appTime: new Date(),
                 content: error
               } as SimpleMessage);
-            
-              // IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
-              // and the background-task may be completed.  You must do this regardless if your operations are successful or not.
-              // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
-              // this.backgroundGeolocation.finish(); // FOR IOS ONLY
-
-              //this.backgroundGeolocation.finish();
             }
           );
 
@@ -384,8 +246,6 @@ export class AppComponent implements OnInit, OnDestroy {
                 appTime: new Date(),
                 content: 'BackgroundGeolocation service has been started'
               } as SimpleMessage);
-
-              //this.backgroundGeolocation.finish();
             }
           );
 
@@ -401,8 +261,6 @@ export class AppComponent implements OnInit, OnDestroy {
                 appTime: new Date(),
                 content: 'BackgroundGeolocation service has been stopped'
               } as SimpleMessage);
-
-              //this.backgroundGeolocation.finish();
             }
           );
 
@@ -418,8 +276,6 @@ export class AppComponent implements OnInit, OnDestroy {
                 appTime: new Date(),
                 content: `BackgroundGeolocation authorization status: ${JSON.stringify(status)}`
               } as SimpleMessage);
-
-              //this.backgroundGeolocation.finish();
             }
           );
 
@@ -435,8 +291,6 @@ export class AppComponent implements OnInit, OnDestroy {
                 appTime: new Date(),
                 content: 'Foreground mode'
               } as SimpleMessage);
-
-              //this.backgroundGeolocation.finish();
             }
           );
 
@@ -452,8 +306,6 @@ export class AppComponent implements OnInit, OnDestroy {
                 appTime: new Date(),
                 content: 'Activity'
               } as SimpleMessage);
-
-              this.backgroundGeolocation.finish();
             }
           );
 
@@ -469,7 +321,6 @@ export class AppComponent implements OnInit, OnDestroy {
                 appTime: new Date(),
                 content: 'HTTP Authorization'
               } as SimpleMessage);
-              //this.backgroundGeolocation.finish();
             }
           );
 
@@ -485,38 +336,10 @@ export class AppComponent implements OnInit, OnDestroy {
                 appTime: new Date(),
                 content: 'Abort Requested'
               } as SimpleMessage);
-
-              //this.backgroundGeolocation.finish();
             }
           );
 
     });
   }
 
-  private initAndConfigureGeofencing() {
-    this.backgroundMode.on('activate')
-      .subscribe(
-        () => {
-          // ELSZÁLL, MERT HIBÁS A PLUGIN EZEN RÉSZE
-          // this.backgroundMode.disableWebViewOptimizations();
-        }
-      )
-
-    // Geolocation.getCurrentPosition({
-    //   enableHighAccuracy: true,
-    //   timeout: 5000
-    // }).then(loc => {
-    //   console.log("[INFO] Location:", JSON.stringify(loc));
-    // }).catch(err => {
-    //   console.error("[ERROR] Error:", JSON.stringify(err));
-    // });
-
-    // Geolocation.watchPosition({
-    //   enableHighAccuracy: true,
-    //   timeout: 5000
-    // }, loc => {
-    //   console.log('[INFO] Background mode:', this.backgroundMode.isEnabled(), this.backgroundMode.isActive());
-    //   console.log('[INFO] Location:', JSON.stringify(loc));
-    // });
-  }
 }
