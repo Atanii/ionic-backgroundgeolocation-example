6113b9c45c6643e9f8ad4ec96a92eda658235d9a
Jeremi <kdrisaac42@gmail.com>
2021-02-18 13:45:15 +0100

    distance calculation and log in km (both stationary and non-stationary)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,6 +34,9 @@ export class AppComponent implements OnInit, OnDestroy {
   public labels = [
     'Geolocation', 'POC', 'Logs', 'Background'
   ];
+
+  public previousLocation: GeoLoc;
+  public previousStatinoaryLocation: GeoLoc;
   
   // BackgroundGeolocationProvider.ANDROID_DISTANCE_FILTER_PROVIDER = 0,
   // BackgroundGeolocationProvider.ANDROID_ACTIVITY_PROVIDER = 1
@@ -47,8 +50,8 @@ export class AppComponent implements OnInit, OnDestroy {
     stationaryRadius: 20,
     distanceFilter: 30,
     
-    interval: 60000,
-    fastestInterval: 60000,
+    interval: 10000,
+    fastestInterval: 10000,
     
     startForeground: true,
     stopOnStillActivity: false,
@@ -124,12 +127,12 @@ export class AppComponent implements OnInit, OnDestroy {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
 
-      this.setBg = false;
-      this.setBgGeoLoc = false;
-      this.setPOST = false;
+      this.setBg = true;
+      this.setBgGeoLoc = true;
+      this.setPOST = true;
       this.setLog = false;
       this.setMiscLog = false;
-      this.setGeoLog = false;
+      this.setGeoLog = true;
       this.setErrorLog = false;
       this.setWakelock = false;
 
@@ -151,23 +154,39 @@ export class AppComponent implements OnInit, OnDestroy {
     console.log('[INFO] AppComponent destroyed')
   }
 
+  public getDistanceFromLatLonInKm(lon1: number, lat1: number, lon2: number, lat2: number): number {
+    const earthR = 6371; // in km
+    const dLat = this.toRad(lat2 - lat1);
+    const dLon = this.toRad(lon2 - lon1);
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(this.toRad(lat1)) * Math.cos(this.toRad(lat2)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = earthR * c; // distance in km
+    return d;
+  }
+
+  public toRad(deg: number): number {
+    return deg * (Math.PI / 180)
+  }
+
   private initAndConfigureBgGeoLoc(): any {
     this.backgroundGeolocation.configure(this.config)
       .then(() => {
-        this.backgroundGeolocation.on(BackgroundGeolocationEvents.background)
-          .pipe(
-            tap(() => console.log("[INFO] Background mode")),
-            filter(() => this.setLog)
-          )
-          .subscribe(
-            () => {
-              this.locService.addNewMsg({
-                pluginTime: new Date(),
-                appTime: new Date(),
-                content: 'Background mode'
-              } as SimpleMessage);
-            }
-          );
+        // this.backgroundGeolocation.on(BackgroundGeolocationEvents.background)
+        //   .pipe(
+        //     tap(() => console.log("[INFO] Background mode")),
+        //     filter(() => this.setLog)
+        //   )
+        //   .subscribe(
+        //     () => {
+        //       this.locService.addNewMsg({
+        //         pluginTime: new Date(),
+        //         appTime: new Date(),
+        //         content: 'Background mode'
+        //       } as SimpleMessage);
+        //     }
+        //   );
 
         this.backgroundGeolocation.on(BackgroundGeolocationEvents.location)
           .pipe(
@@ -176,32 +195,29 @@ export class AppComponent implements OnInit, OnDestroy {
           .subscribe(
             location => {
               if (this.setGeoLog) {
-                this.locService.addNewLoc({
+                const tmp = {
                   pluginTime: new Date(location.time),
                   appTime: new Date(),
                   longitude: location.longitude,
                   latitude: location.latitude
-                } as GeoLoc);
+                } as GeoLoc;
+
+                this.locService.addNewLoc(tmp);
+
+                if (this.previousLocation) {
+                  const dist = this.getDistanceFromLatLonInKm(
+                    tmp.longitude, tmp.latitude,
+                    this.previousLocation.longitude, this.previousLocation.latitude
+                  );
+                  this.locService.addNewMsg({
+                    pluginTime: new Date(location.time),
+                    appTime: new Date(),
+                    content: `Dist: ${dist}`
+                  } as SimpleMessage);
+                }
+
+                this.previousLocation = tmp;
               }
-
-              if (this.setLog) {
-                this.locService.addNewMsg({
-                  pluginTime: new Date(location.time),
-                  appTime: new Date(),
-                  content: '[INFO][BackgroundGeolocationEvents.location] Location updated'
-                } as SimpleMessage);
-              }
-
-              ///////////////////////////////////////// POST
-              // this.locService.postNewLocation({
-              //   pluginTime: new Date(location.time),
-              //   appTime: new Date(),
-              //   longitude: location.longitude,
-              //   latitude: location.latitude,
-              //   msg: 'BackgroundGeolocationEvents.location'
-              // } as GeoLoc);
-
-              // this.backgroundGeolocation.finish();
             }
           );
 
@@ -212,141 +228,157 @@ export class AppComponent implements OnInit, OnDestroy {
           .subscribe(
             location => {
               if (this.setGeoLog) {
-                this.locService.addNewLoc({
+                const tmp = {
                   pluginTime: new Date(location.time),
                   appTime: new Date(),
                   longitude: location.longitude,
                   latitude: location.latitude
-                } as GeoLoc);
+                } as GeoLoc;
+
+                this.locService.addNewLoc(tmp);
+
+                if (this.previousStatinoaryLocation) {
+                  const dist = this.getDistanceFromLatLonInKm(
+                    tmp.longitude, tmp.latitude,
+                    this.previousStatinoaryLocation.longitude, this.previousStatinoaryLocation.latitude
+                  );
+                  this.locService.addNewMsg({
+                    pluginTime: new Date(location.time),
+                    appTime: new Date(),
+                    content: `Stationary dist: ${dist}`
+                  } as SimpleMessage);
+                }
+
+                this.previousStatinoaryLocation = tmp;
               }
             }
           );
 
-        this.backgroundGeolocation.on(BackgroundGeolocationEvents.error)
-          .pipe(
-            map(error => `Error: ${JSON.stringify(error)}`),
-            tap(error => console.error('[ERROR]', error)),
-            filter(_ => this.setErrorLog)
-          )
-          .subscribe(
-            error => {
-              this.locService.addNewMsg({
-                pluginTime: new Date(),
-                appTime: new Date(),
-                content: error
-              } as SimpleMessage);
+        // this.backgroundGeolocation.on(BackgroundGeolocationEvents.error)
+        //   .pipe(
+        //     map(error => `Error: ${JSON.stringify(error)}`),
+        //     tap(error => console.error('[ERROR]', error)),
+        //     filter(_ => this.setErrorLog)
+        //   )
+        //   .subscribe(
+        //     error => {
+        //       this.locService.addNewMsg({
+        //         pluginTime: new Date(),
+        //         appTime: new Date(),
+        //         content: error
+        //       } as SimpleMessage);
             
-              // IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
-              // and the background-task may be completed.  You must do this regardless if your operations are successful or not.
-              // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
-              // this.backgroundGeolocation.finish(); // FOR IOS ONLY
-            }
-          );
+        //       // IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
+        //       // and the background-task may be completed.  You must do this regardless if your operations are successful or not.
+        //       // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+        //       // this.backgroundGeolocation.finish(); // FOR IOS ONLY
+        //     }
+        //   );
 
-        this.backgroundGeolocation.on(BackgroundGeolocationEvents.start)
-          .pipe(
-            tap(() => console.log('[INFO] BackgroundGeolocation service has been started')),
-            filter(() => this.setMiscLog)
-          )
-          .subscribe(
-            () => {
-              this.locService.addNewMsg({
-                pluginTime: new Date(),
-                appTime: new Date(),
-                content: 'BackgroundGeolocation service has been started'
-              } as SimpleMessage);
-            }
-          );
+        // this.backgroundGeolocation.on(BackgroundGeolocationEvents.start)
+        //   .pipe(
+        //     tap(() => console.log('[INFO] BackgroundGeolocation service has been started')),
+        //     filter(() => this.setMiscLog)
+        //   )
+        //   .subscribe(
+        //     () => {
+        //       this.locService.addNewMsg({
+        //         pluginTime: new Date(),
+        //         appTime: new Date(),
+        //         content: 'BackgroundGeolocation service has been started'
+        //       } as SimpleMessage);
+        //     }
+        //   );
 
-        this.backgroundGeolocation.on(BackgroundGeolocationEvents.stop)
-          .pipe(
-            tap(() => console.log('[INFO] BackgroundGeolocation service has been stopped')),
-            filter(() => this.setMiscLog)
-          )
-          .subscribe(
-            () => {
-              this.locService.addNewMsg({
-                pluginTime: new Date(),
-                appTime: new Date(),
-                content: 'BackgroundGeolocation service has been stopped'
-              } as SimpleMessage);
-            }
-          );
+        // this.backgroundGeolocation.on(BackgroundGeolocationEvents.stop)
+        //   .pipe(
+        //     tap(() => console.log('[INFO] BackgroundGeolocation service has been stopped')),
+        //     filter(() => this.setMiscLog)
+        //   )
+        //   .subscribe(
+        //     () => {
+        //       this.locService.addNewMsg({
+        //         pluginTime: new Date(),
+        //         appTime: new Date(),
+        //         content: 'BackgroundGeolocation service has been stopped'
+        //       } as SimpleMessage);
+        //     }
+        //   );
 
-        this.backgroundGeolocation.on(BackgroundGeolocationEvents.authorization)
-          .pipe(
-            tap(status => console.log('[INFO] BackgroundGeolocation authorization status:', JSON.stringify(status))),
-            filter(() => this.setMiscLog)
-          )
-          .subscribe(
-            status => {
-              this.locService.addNewMsg({
-                pluginTime: new Date(status.time),
-                appTime: new Date(),
-                content: `BackgroundGeolocation authorization status: ${JSON.stringify(status)}`
-              } as SimpleMessage);
-            }
-          );
+        // this.backgroundGeolocation.on(BackgroundGeolocationEvents.authorization)
+        //   .pipe(
+        //     tap(status => console.log('[INFO] BackgroundGeolocation authorization status:', JSON.stringify(status))),
+        //     filter(() => this.setMiscLog)
+        //   )
+        //   .subscribe(
+        //     status => {
+        //       this.locService.addNewMsg({
+        //         pluginTime: new Date(status.time),
+        //         appTime: new Date(),
+        //         content: `BackgroundGeolocation authorization status: ${JSON.stringify(status)}`
+        //       } as SimpleMessage);
+        //     }
+        //   );
 
-        this.backgroundGeolocation.on(BackgroundGeolocationEvents.foreground)
-          .pipe(
-            tap(() => console.log('[INFO] Foreground mode')),
-            filter(() => this.setMiscLog)
-          )
-          .subscribe(
-            () => {
-              this.locService.addNewMsg({
-                pluginTime: new Date(),
-                appTime: new Date(),
-                content: 'Foreground mode'
-              } as SimpleMessage);
-            }
-          );
+        // this.backgroundGeolocation.on(BackgroundGeolocationEvents.foreground)
+        //   .pipe(
+        //     tap(() => console.log('[INFO] Foreground mode')),
+        //     filter(() => this.setMiscLog)
+        //   )
+        //   .subscribe(
+        //     () => {
+        //       this.locService.addNewMsg({
+        //         pluginTime: new Date(),
+        //         appTime: new Date(),
+        //         content: 'Foreground mode'
+        //       } as SimpleMessage);
+        //     }
+        //   );
 
-        this.backgroundGeolocation.on(BackgroundGeolocationEvents.activity)
-          .pipe(
-            tap(() => console.log('[INFO] Activity')),
-            filter(() => this.setMiscLog)
-          )
-          .subscribe(
-            () => {
-              this.locService.addNewMsg({
-                pluginTime: new Date(),
-                appTime: new Date(),
-                content: 'Activity'
-              } as SimpleMessage);
-            }
-          );
+        // this.backgroundGeolocation.on(BackgroundGeolocationEvents.activity)
+        //   .pipe(
+        //     tap(() => console.log('[INFO] Activity')),
+        //     filter(() => this.setMiscLog)
+        //   )
+        //   .subscribe(
+        //     () => {
+        //       this.locService.addNewMsg({
+        //         pluginTime: new Date(),
+        //         appTime: new Date(),
+        //         content: 'Activity'
+        //       } as SimpleMessage);
+        //     }
+        //   );
 
-        this.backgroundGeolocation.on(BackgroundGeolocationEvents.http_authorization)
-          .pipe(
-            tap(() => console.log('[INFO] HTTP Authorization')),
-            filter(() => this.setMiscLog)
-          )
-          .subscribe(
-            () => {
-              this.locService.addNewMsg({
-                pluginTime: new Date(),
-                appTime: new Date(),
-                content: 'HTTP Authorization'
-              } as SimpleMessage);
-            }
-          );
+        // this.backgroundGeolocation.on(BackgroundGeolocationEvents.http_authorization)
+        //   .pipe(
+        //     tap(() => console.log('[INFO] HTTP Authorization')),
+        //     filter(() => this.setMiscLog)
+        //   )
+        //   .subscribe(
+        //     () => {
+        //       this.locService.addNewMsg({
+        //         pluginTime: new Date(),
+        //         appTime: new Date(),
+        //         content: 'HTTP Authorization'
+        //       } as SimpleMessage);
+        //     }
+        //   );
 
-        this.backgroundGeolocation.on(BackgroundGeolocationEvents.abort_requested)
-          .pipe(
-            tap(() => console.log('[INFO] Abort Requested')),
-            filter(() => this.setMiscLog)
-          )
-          .subscribe(
-            () => {
-              this.locService.addNewMsg({
-                pluginTime: new Date(),
-                appTime: new Date(),
-                content: 'Abort Requested'
-              } as SimpleMessage);
-            }
-          );
+        // this.backgroundGeolocation.on(BackgroundGeolocationEvents.abort_requested)
+        //   .pipe(
+        //     tap(() => console.log('[INFO] Abort Requested')),
+        //     filter(() => this.setMiscLog)
+        //   )
+        //   .subscribe(
+        //     () => {
+        //       this.locService.addNewMsg({
+        //         pluginTime: new Date(),
+        //         appTime: new Date(),
+        //         content: 'Abort Requested'
+        //       } as SimpleMessage);
+        //     }
+        //   );
 
     });
   }
